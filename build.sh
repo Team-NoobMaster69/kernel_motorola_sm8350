35dadc7e139ad69c22ae0c15887035c658b28353
HotaruOs <arroztlg@gmail.com>
2025-12-08 10:44:36 -0300

    you're not welcome  Fck HuP

--- build.sh
+++ build.sh
@@ -6,22 +6,21 @@ objdir="${kernel_dir}/out"
 anykernel=$HOME/anykernel
 builddir="${kernel_dir}/build"
 ZIMAGE=$kernel_dir/out/arch/arm64/boot/Image
-kernel_name="torracat-tundra"
+kernel_name="HuP-Fck死"
 zip_name="$kernel_name-$(date +"%d%m%Y-%H%M").zip"
-TC_DIR=$HOME/tc
-CLANG_DIR=$TC_DIR/clang-r522817
-export CONFIG_FILE="vendor/tundra-qgki_defconfig"
+TC_DIR="${PWD}/tc"
+export CONFIG_FILE="vendor/lahaina-qgki_defconfig"
 export ARCH="arm64"
-export KBUILD_BUILD_HOST=raghavt20
-export KBUILD_BUILD_USER=raghav
+export KBUILD_BUILD_HOST=Pão
+export KBUILD_BUILD_USER=HotaruOs
 
-export PATH="$CLANG_DIR/bin:$PATH"
+export PATH="$TC_DIR/bin:$PATH"
 if ! [ -d "$TC_DIR" ]; then
-    echo "Toolchain not found! Cloning to $TC_DIR..."
-    if ! git clone -q --depth=1 --single-branch https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 -b master $TC_DIR; then
-        echo "Cloning failed! Aborting..."
-        exit 1
-    fi
+	echo "AOSP clang not found! Cloning to $TC_DIR..."
+	if ! git clone --depth=1 -b 18 https://gitlab.com/ThankYouMario/android_prebuilts_clang-standalone "$TC_DIR"; then
+		echo "Cloning failed! Aborting..."
+		exit 1
+	fi
 fi
 
 # Colors
@@ -29,6 +28,9 @@ NC='\033[0m'
 RED='\033[0;31m'
 LRD='\033[1;31m'
 LGR='\033[1;32m'
+
+echo -e "${LGR}######### Versão do Clang #########${NC}"
+$TC_DIR/bin/clang --version
 
 make_defconfig() {
     START=$(date +"%s")
@@ -58,7 +60,7 @@ completion() {
     COMPILED_DTB=arch/arm64/boot/dts/vendor/qcom/lahaina*dtb
     if [[ -f ${COMPILED_IMAGE} ]]; then
 
-        git clone -q https://github.com/raghavt20/AnyKernel3 -b tundra $anykernel
+        git clone -q https://github.com/HotaruOs/AnyKernel3 -b pão $anykernel
 
         cp -f ${COMPILED_IMAGE} $anykernel
         cp -f "${DTB_DIR}"/*.img $anykernel
@@ -69,18 +71,19 @@ completion() {
         find . -name "*.zip" -type f -delete
         zip -r AnyKernel.zip *
         cp AnyKernel.zip $zip_name
-        cp $anykernel/$zip_name $HOME/$zip_name
+        cp $anykernel/$zip_name $kernel_dir/$zip_name
         rm -rf $anykernel
         END=$(date +"%s")
         DIFF=$(($END - $START))
-        curl -F "file=@$HOME/$zip_name" https://temp.sh/upload
-        echo -e ${LGR} "############################################"
-        echo -e ${LGR} "############# OkThisIsEpic!  ##############"
-        echo -e ${LGR} "############################################${NC}"
+        curl -F "file=@$kernel_dir/$zip_name" https://temp.sh/upload
+	echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
+        echo -e ${LGR} "#############################################"
+        echo -e ${LGR} "####### Kernel compiled successfully ########"
+        echo -e ${LGR} "#############################################${NC}"
     else
-        echo -e ${RED} "############################################"
-        echo -e ${RED} "##         This Is Not Epic :'(           ##"
-        echo -e ${RED} "############################################${NC}"
+        echo -e ${RED} "#############################################"
+        echo -e ${RED} "######## Failed to compile Kernel #########"
+        echo -e ${RED} "#############################################${NC}"
     fi
 }
 
